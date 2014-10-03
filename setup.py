bf82a5622798dfeb9fc9ccaeb20194fb90449cff
Daniel Brandt <me@dbrandt.se>
2014-10-03 20:44:32 +0200

    more package setup.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+kwargs = {
+    "name": "wxrtools",
+    "version": "0.1-prerelease",
+    "description": "Package for handling WordPress export format (WXR)",
+    "author": "Daniel Brandt",
+    "author_email": "me@dbrandt.se",
+    "url": "http://dbrandt.se/wxrtools",
+    "scripts": [],
+    "packages": find_packages(),
+    "install_requires": [l.strip() for l in open("requirements.txt").readlines()],
+}
+
+if __name__ == "__main__":
+    setup(**kwargs)
